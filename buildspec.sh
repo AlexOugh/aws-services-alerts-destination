01c5fdf10614b4683346e460a704980a642661f4
Alex Ough <alex.ough@sungardas.com>
2018-03-29 19:16:33 -0500

    initial commit

---
+++ buildspec.sh
@@ -0,0 +1,12 @@
+
+source .env.local
+
+aws cloudformation package \
+   --template-file ./template.yaml \
+   --s3-bucket $S3_BUCKET_NAME \
+   --output-template-file samTemplate.yaml
+
+
+aws cloudformation deploy --template-file ./samTemplate.yaml \
+  --capabilities CAPABILITY_IAM \
+  --stack-name SungardAS-aws-services-alerts-destination
